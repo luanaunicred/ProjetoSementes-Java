0cb4077aa3cfbb82778aa4c38c6cf896cbd3f992
luana.souza <luana.souza@e-unicred.com.br>
2025-05-07 08:59:59 -0300

    Ajuste desafio 14 - usando vetores.

--- Desafio14.java
+++ Desafio14.java
@@ -7,14 +7,28 @@ public class Desafio14 {
                 "Marley", "Mel", "Ceci", "Maria", "Andrea", "Carmen", "Joana", "Luana", "Rita"
         };
 
-        HashSet<String> nomesUnicos = new HashSet<>();
-        for (String nome : nomes) {
-            nomesUnicos.add(nome);
+        String[] nomesUnicos = new String[nomes.length];
+        int contador = 0;
+
+        for (int i = 0; i < nomes.length; i++) {
+            boolean repetido = false;
+
+            for (int j = 0; j < contador; j++) {
+                if (nomes[i].equals(nomesUnicos[j])) {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            if (!repetido) {
+                nomesUnicos[contador] = nomes[i];
+                contador++;
+            }
         }
 
         System.out.println("Nomes únicos após remover duplicatas: ");
-        for (String nome : nomesUnicos) {
-            System.out.println(nome);
+        for (int i = 0; i < contador; i++) {
+            System.out.println(nomesUnicos[i]);
         }
     }
 }
